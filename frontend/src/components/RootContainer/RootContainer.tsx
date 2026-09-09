ce569c6b647e6678e055fb959ae82dffe4611e9c
dudu_win10 <35301543+githubdudu@users.noreply.github.com>
2026-09-09 21:43:47 +1200

    ✨ feat(Rootlayout): iphone size

--- frontend/src/components/RootContainer/RootContainer.tsx
+++ frontend/src/components/RootContainer/RootContainer.tsx
@@ -9,13 +9,13 @@ interface RootContainerProps {
  * for the application. It sets the height to fill the screen and applies
  * a background color.
  *
- * It sets layout for screen sizes: 20rem/320px, 48rem/768px and rest.
+ * It sets layout for screen sizes: 20rem/320px and iphone 14/15 similar 390×844
  *
  * It is used in Home and LogoLayout components.
  */
 function RootContainer({ children }: RootContainerProps) {
   return (
-    <div className="flex h-screen min-w-xs flex-col bg-gray-100 md:mx-auto md:max-w-3xl">
+    <div className="mx-auto flex h-screen max-h-211 min-w-xs flex-col overflow-clip bg-gray-100 md:my-auto md:max-w-97.5 md:rounded-2xl md:shadow-xl">
       {children}
     </div>
   );
